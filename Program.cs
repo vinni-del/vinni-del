1e2e7ceac787f12c3dea4c38eafd1beda655dc14
vinni-del <59853693+vinni-del@users.noreply.github.com>
2021-11-27 10:58:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArraySUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10]{1,2,3,4,5};
+            System.Console.WriteLine(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+            arr[i] = console.ReadLine();
+            System.Console.WriteLine(arr[i] "+");//sum ?
+
+        }
+    }
+}
